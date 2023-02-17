177ddabd2d74534c88369880d29b6784b037edd2
Kelvin <gilkelvin2@gmail.com>
2023-02-16 20:40:41 -0500

    commit tree ebf2e1465c813016b5d0c5d41aff55b7e0c182be

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = [
+    "Milo","Otis","Garfield",
+];
+
+function destructivelyAppendCat(name){
+    cats.push(name);   
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+function appendCat(name){
+    const newCats = [...cats,name];
+    return newCats;
+}
+
+function prependCat(name){
+    const newCat = [name,...cats];
+    return newCat;
+}
+function removeLastCat(name){
+    const newestCat = [...cats.slice(0,cats.length-1)];
+    return newestCat;
+}
+function removeFirstCat(name){
+    const catsTwo = [...cats.slice(1,cats.length)];
+    return catsTwo;
+}
+
+
